2b9369892f5d5fb25b8d80a7344ff84a53f1cd78
Truglut <contrerasdesantos.andres@gmail.com>
2026-05-08 15:14:54 +0200

    feat: add readable labels to Space enum

--- utils/space.py
+++ utils/space.py
@@ -1,7 +1,18 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class Space(Enum):
-    REAL = 1
-    FOURIER_REAL = 2
-    FOURIER_IMAG = 3
+    REAL = auto()
+    FOURIER_REAL = auto()
+    FOURIER_IMAG = auto()
+
+    @property
+    def label(self):
+        return{
+            Space.REAL: "Real Space",
+            Space.FOURIER_REAL: "Fourier Space (Real Part)",
+            Space.FOURIER_IMAG: "Fourier Space (Imaginary Part)"
+        }[self]
+
+    def __str__(self):
+        return self.label
