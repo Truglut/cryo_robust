3b24301379ddbfc3d01c0055dad059a9d8b9f2e2
Truglut <contrerasdesantos.andres@gmail.com>
2026-09-01 18:25:27 +0200

    add max_iter property and space parameter in reconstruct_from_weights to base Estimator class

--- src/cryo_robust/estimators/base.py
+++ src/cryo_robust/estimators/base.py
@@ -1,17 +1,19 @@
 import torch
 
+from cryo_robust.domain import ImageSpace
+
 from .data import ImageBatch
 from .results import EstimatorResult, WeightSet
 
 
 class Estimator:
+    max_iter: int
+
     def fit(self, images: ImageBatch) -> EstimatorResult:
         raise NotImplementedError("Subclasses must implement the fit method.")
 
     def reconstruct_from_weights(
-        self,
-        images: ImageBatch,
-        weights: WeightSet,
+        self, images: ImageBatch, weights: WeightSet, space: ImageSpace
     ) -> torch.Tensor:
         raise NotImplementedError(
             "Subclasses must implement the reconstruct from weights method"
